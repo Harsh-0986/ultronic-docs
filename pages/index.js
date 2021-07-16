0255fee1f4d9f1905a8b98dc66ad1de63feeb5d0
Harsh Shah <harsh.r.shah2102@gmail.com>
2021-07-16 09:51:43 +0530

    Completed the body

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
+import Button from "@material-tailwind/react/Button";
 import Head from "next/head";
 import Header from "../components/Header";
+import Icon from "@material-tailwind/react/Icon";
+import Image from "@material-tailwind/react/Image";
 
 export default function Home() {
   return (
@@ -11,7 +14,42 @@ export default function Home() {
 
       <Header />
 
-      <section></section>
+      <section className="bg-[#F8F9FA] pb-10 px-10">
+        <div className="max-w-3xl mx-auto">
+          <div className="flex items-center justify-between py-6">
+            <h2 className="text-gray-700 text-md">Start a new document</h2>
+
+            <Button
+              color="gray"
+              buttonType="outline"
+              iconOnly={true}
+              ripple="dark"
+              className="border-0"
+            >
+              <Icon name="more_vert" size="10l"></Icon>
+            </Button>
+          </div>
+          <div>
+            <div className="relative h-53 w-40 border-2 cursor-pointer hover:border-blue-700">
+              <Image src="https://links.papareact.com/pju" layout="fill" />
+            </div>
+
+            <p className="ml-2 mt-2 font-semibold text-sm text-gray-700 text-left">
+              Blank
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="bg-white px-10 md:px-0">
+        <div className="max-w-3xl mx-auto py-8 text-sm text-gray-700">
+          <div className="flex items-center justify-between pb-5">
+            <h2 className="font-medium flex-grow">My Documents</h2>
+            <p className="mr-12">Date Created</p>
+            <Icon name="folder" size="10l" color="gray" />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
