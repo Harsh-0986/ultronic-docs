0aed17cd0978ed86dfba527191e4d8358f3f1e28
Harsh-0986 <support@harsh-shah.tk>
2022-09-08 08:35:54 +0000

    Fixed some bugs

--- pages/doc/[id].js
+++ pages/doc/[id].js
@@ -6,6 +6,7 @@ import { useDocumentOnce } from "react-firebase-hooks/firestore";
 import { getSession, useSession, signOut } from "next-auth/client";
 import Login from "../../components/Login";
 import TextEditor from "../../components/TextEditor";
+import Head from "next/head";
 
 function Doc() {
   const [session] = useSession();
@@ -26,47 +27,53 @@ function Doc() {
   if (!session) return <Login />;
 
   return (
-    <div>
-      <header className="flex justify-between items-center p-3 pb-1">
-        <span onClick={() => router.push("/")} className="cursor-pointer">
-          <Icon name="description" size="5xl" color="gray" />
-        </span>
+    <>
+      <Head>
+        <title>{snapshot?.data()?.fileName}</title>
+      </Head>
 
-        <div className="flex-grow px-2">
-          <h2>{snapshot?.data()?.fileName}</h2>
-          <div className="flex items-center text-sm space-x-1 -ml-1 h-8 text-gray-600">
-            <p className="option">File</p>
-            <p className="option">Edit</p>
-            <p className="option">View</p>
-            <p className="option">Insert</p>
-            <p className="option">Format</p>
-            <p className="option">Tools</p>
+      <div>
+        <header className="flex justify-between items-center p-3 pb-1">
+          <span onClick={() => router.push("/")} className="cursor-pointer">
+            <Icon name="description" size="5xl" color="gray" />
+          </span>
+
+          <div className="flex-grow px-2">
+            <h2>{snapshot?.data()?.fileName}</h2>
+            <div className="flex items-center text-sm space-x-1 -ml-1 h-8 text-gray-600">
+              <p className="option">File</p>
+              <p className="option">Edit</p>
+              <p className="option">View</p>
+              <p className="option">Insert</p>
+              <p className="option">Format</p>
+              <p className="option">Tools</p>
+            </div>
           </div>
-        </div>
 
-        <Button
-          color="lightBlue"
-          buttonType="filled"
-          size="regular"
-          className="hidden md:inline-flex h-10"
-          rounded={false}
-          block={false}
-          iconOnly={false}
-          ripple="light"
-        >
-          <Icon name="people" size="md" />
-          SHARE
-        </Button>
+          <Button
+            color="lightBlue"
+            buttonType="filled"
+            size="regular"
+            className="hidden md:inline-flex h-10"
+            rounded={false}
+            block={false}
+            iconOnly={false}
+            ripple="light"
+          >
+            <Icon name="people" size="md" />
+            SHARE
+          </Button>
 
-        <img
-          src={session.user.image}
-          alt=""
-          className=" cursor-pointer h-10 w-10 ml-2 rounded-full"
-        />
-      </header>
+          <img
+            src={session.user.image}
+            alt=""
+            className=" cursor-pointer h-10 w-10 ml-2 rounded-full"
+          />
+        </header>
 
-      <TextEditor />
-    </div>
+        <TextEditor />
+      </div>
+    </>
   );
 }
 
